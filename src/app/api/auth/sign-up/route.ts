6090cec5ffae341d288ffb19edc064cd7aa1bd7d
Vladislav Krutikov <vladislav.k@interexy.com>
2024-09-23 21:03:49 +0300

    fix

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,7 +1,8 @@
+import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
 import { NextResponse } from 'next/server'
 
-import prisma from 'lib/prisma'
+const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
   const { email, password, name } = await request.json()
